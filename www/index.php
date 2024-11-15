ad232c5060032d577a992ecf5965ee200c13c928
drodriguez357 <drodriguez1109@alumnes.politecnicllevant.cat>
2024-11-15 12:10:59 +0100

    ep 5

--- www/index.php
+++ www/index.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
 <body>
+    <?php
+        $book = "El principito";
+        $readed = false;
+            
+        if ($readed) {
+            $message = "Ha leido $book";
+        } else {
+            $message = "NO ha leido $book";
+        }
+    ?>
     <h1>
-        <?php
-            $saludar = "Hola";
-            echo "$saludar mundo";
-        ?>
+        <?= $message ?>
     </h1>
 </body>
 </html>
