e972c39a9ef030607d5f409f0c171932433a827e
daniel <d.r.ferrer8@gmail.com>
2024-11-24 23:29:53 +0100

    ep 8

--- www/index.php
+++ www/index.php
@@ -23,15 +23,31 @@
                 "title" => "El Quijote",
                 "author" => "Cerbantes",
                 "url" => "http://El_Quijote"
+            ],
+            [
+                "title" => "Alicia en el pais de las maravillas",
+                "author" => "nose",
+                "url" => "http://Alicia"
             ]
         ];
+        function filterByAuthor($books, $author){
+            $filteredBooks = [];
+            foreach ($books as $book) {
+                if($book['author'] === $author){
+                    $filteredBooks[] = $book;
+                }
+            }     
+            return $filteredBooks;       
+        }
+
     ?>
+    
     <ul>
-        <?php foreach ($books as $book) : ?>
-            <a href="<?= $book['url'] ?>">
-                <li> <?= $book['title'] ?> </li>
-            </a>
-                <?php endforeach; ?>
+        <?php foreach (filterByAuthor($books, 'nose') as $book) : ?>
+                <a href="<?= $book['url'] ?>">
+                    <li> <?= $book['title'] ?> </li>
+                </a>
+        <?php endforeach; ?>
 
     </ul>
 </body>
