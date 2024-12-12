da6752d8f0e14875b426754da09c0fa7ec2888c5
daniel <d.r.ferrer8@gmail.com>
2024-12-12 16:12:03 +0100

    ep 10

--- www/index.php
+++ www/index.php
@@ -1,65 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
-</head>
-<body>
-    <?php
-        $books = [
-            [
-                "title" => "La odisea",
-                "author" => "Homero",
-                "url" => "http://la_odisea",
-                "year" => 1234
-            ],
-            [
-                "title" => "La dama de las camelias",
-                "author" => "nose",
-                "url" => "http://la_dama",
-                "year" => 2345
-            ],
-            [
-                "title" => "El Quijote",
-                "author" => "Cerbantes",
-                "url" => "http://El_Quijote",
-                "year" => 1637
-            ],
-            [
-                "title" => "Alicia en el pais de las maravillas",
-                "author" => "nose",
-                "url" => "http://Alicia",
-                "year" => 3425
-            ]
-        ];
-        
-        //No se usa !!!
-        function filter($items, $fn){
-            $filteredItems = [];
-            foreach ($items as $item) {
-                if($fn($item)){
-                    $filteredItems[] = $item;
-                }
-            }     
-            return $filteredItems;       
+<?php
+$books = [
+    [
+        "title" => "La odisea",
+        "author" => "Homero",
+        "url" => "http://la_odisea",
+        "year" => 1234
+    ],
+    [
+        "title" => "La dama de las camelias",
+        "author" => "nose",
+        "url" => "http://la_dama",
+        "year" => 2345
+    ],
+    [
+        "title" => "El Quijote",
+        "author" => "Cerbantes",
+        "url" => "http://El_Quijote",
+        "year" => 1637
+    ],
+    [
+        "title" => "Alicia en el pais de las maravillas",
+        "author" => "nose",
+        "url" => "http://Alicia",
+        "year" => 3425
+    ]
+];
+
+//No se usa !!!
+function filter($items, $fn){
+    $filteredItems = [];
+    foreach ($items as $item) {
+        if($fn($item)){
+            $filteredItems[] = $item;
         }
+    }
+    return $filteredItems;
+}
 
 
-        $filteredBooks = array_filter($books, function ($book) {
-            return $book['year'] === 1234;
-        })
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['year'] === 3425;
+});
 
-    ?>
-    
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-                <a href="<?= $book['url'] ?>">
-                    <li> <?= $book['title'] ?> </li>
-                </a>
-        <?php endforeach; ?>
-
-    </ul>
-</body>
-</html>
+require "index.view.php";
