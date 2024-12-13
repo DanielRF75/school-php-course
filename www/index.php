8a447b4b16c7fcd798cec468d9e1e017f599d6b8
daniel <d.r.ferrer8@gmail.com>
2024-12-13 09:32:07 +0100

    ep 12

--- www/index.php
+++ www/index.php
@@ -1,15 +1,3 @@
 <?php
-$buisness = [
-    "name" => "PonchoCorp",
-    "cost" => "20",
-    "categorys" => [
-        "testing",
-        "java script",
-        "php"
-        ]
-    ];
-function regsiterUsr($user)
-{
 
-}
 require "index.view.php";
