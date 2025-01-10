8bdc5d7bc60f70f32b45025e380101d3317f1c16
daniel <d.r.ferrer8@gmail.com>
2025-01-10 12:20:51 +0100

    ep 17

--- www/index.php
+++ www/index.php
@@ -1,4 +1,17 @@
 <?php
 require "functions.php";
-require "router.php";
+//require "router.php";
 
+$dns = "mysql:host=contendor_mysql;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dns, "daniel", "dsecret");
+
+$statements = $pdo -> prepare("SELECT * FROM posts");
+
+$statements -> execute();
+
+$posts = $statements->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
